5036ddad72997c9c608f2ce0ae90277e20d8680a
DESKTOP-L6JEFV7\ERP <alsgud0728@gmail.com>
2023-09-08 15:35:31 +0900

    대시보드 매출조회

--- src/main/java/com/example/eatmeserver/seller/dashboard/DashboardController.java
+++ src/main/java/com/example/eatmeserver/seller/dashboard/DashboardController.java
@@ -1,6 +1,9 @@
 package com.example.eatmeserver.seller.dashboard;
 
+import com.example.eatmeserver.seller.dashboard.model.DashboardParam;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,4 +11,10 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 @RequestMapping("/api/dashboard")
 public class DashboardController {
+    private final DashboardService service;
+
+    @PostMapping("/selling")
+    public int getSelling(@RequestBody DashboardParam param) {
+        return service.getSelling(param);
+    }
 }
--- src/main/java/com/example/eatmeserver/seller/dashboard/DashboardService.java
+++ src/main/java/com/example/eatmeserver/seller/dashboard/DashboardService.java
@@ -1,9 +1,25 @@
 package com.example.eatmeserver.seller.dashboard;
 
+import com.example.eatmeserver.seller.dashboard.model.DashboardParam;
+import com.example.eatmeserver.seller.dashboard.model.DashboardSellingFlex;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class DashboardService {
+
+    private final DashboardMapper mapper;
+
+    /** 매출조회 */
+    public int getSelling(DashboardParam param){
+        List<DashboardSellingFlex> result = mapper.getSelling(param);
+        int sum = 0;
+        for (int i = 0; i < result.size(); i++) {
+            sum += result.get(i).getSellPrc();
+        }
+        return sum;
+    }
 }
