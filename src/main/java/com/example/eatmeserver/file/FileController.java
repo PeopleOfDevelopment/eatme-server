5d27e3e6ec58d4129fa36588663825e0245c7106
alsgud2659 <alsgud2659@naver.com>
2023-10-29 22:25:57 +0900

    이미지 업로드 2차 커밋

--- src/main/java/com/example/eatmeserver/file/FileController.java
+++ src/main/java/com/example/eatmeserver/file/FileController.java
@@ -1,11 +1,28 @@
 package com.example.eatmeserver.file;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.util.List;
+import java.util.UUID;
 
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("/api/file")
 public class FileController {
+    @PostMapping("/upload")
+    public void uploadFile(@RequestPart MultipartFile image) throws Exception {
+        String uploadPath = System.getProperty("user.dir") + "\\src\\main\\resource\\static\\image";
+        UUID uuid = UUID.randomUUID();
+        String fileName = uuid + "_" + image.getOriginalFilename();
+        File saveFile = new File(uploadPath, "name");
+
+        image.transferTo(saveFile);
+    }
+
 }
--- src/main/java/com/example/eatmeserver/file/FileMapper.java
+++ src/main/java/com/example/eatmeserver/file/FileMapper.java
@@ -4,4 +4,10 @@ import org.apache.ibatis.annotations.Mapper;
 
 @Mapper
 public interface FileMapper {
+
+    // 이미지 정보 저장
+    int insertImg(FileFlex flex);
+
+    // 이미지 정보 조회
+    FileFlex selectImg(FileParam param);
 }
