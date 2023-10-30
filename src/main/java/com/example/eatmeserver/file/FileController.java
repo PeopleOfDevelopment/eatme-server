6d05b5491b049be38a1a5b6980b460813ca3a12e
DESKTOP-L6JEFV7\ERP <alsgud0728@gmail.com>
2023-10-30 17:02:07 +0900

    이미지 업로드 3차커밋

--- src/main/java/com/example/eatmeserver/file/FileController.java
+++ src/main/java/com/example/eatmeserver/file/FileController.java
@@ -1,10 +1,9 @@
 package com.example.eatmeserver.file;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestPart;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.File;
@@ -15,14 +14,18 @@ import java.util.UUID;
 @RestController
 @RequestMapping("/api/file")
 public class FileController {
-    @PostMapping("/upload")
-    public void uploadFile(@RequestPart MultipartFile image) throws Exception {
-        String uploadPath = System.getProperty("user.dir") + "\\src\\main\\resource\\static\\image";
-        UUID uuid = UUID.randomUUID();
-        String fileName = uuid + "_" + image.getOriginalFilename();
-        File saveFile = new File(uploadPath, fileName);
 
-        image.transferTo(saveFile);
+    private final FileService service;
+
+    @PostMapping("/upload")
+    public void uploadFile(@RequestPart MultipartFile image, @RequestBody FileFlex flex) throws Exception {
+        service.saveImg(flex, image);
     }
+
+    @PostMapping("/getImg/{corpCd}/{itemCd}")
+    public ResponseEntity<byte[]> getImage(@PathVariable String corpCd, @PathVariable String itemCd) throws Exception{
+        return service.getImage(corpCd, itemCd);
+    }
+
 
 }
